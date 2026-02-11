c44e4899682e33c6f636bf791c0cd631184c772c
SYAM <145249375+Syam7780@users.noreply.github.com>
2026-02-11 15:22:58 +0530

    Update app.py

--- Project Files/app.py
+++ Project Files/app.py
@@ -9,7 +9,7 @@ xg_scaler = joblib.load("xg_scaler.pkl")
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predict_page')
 def predict_page():
